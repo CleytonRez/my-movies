07afb55496a3390dd42e2351c294a2c4a85642db
Cleyton Eduardo Rezende <cleytonmgrezendedev@gmail.com>
2021-12-23 18:00:05 -0300

    feature: Create MyMovies Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom'
+
+import {
+  NavButton,
+  NavLink
+} from "reactstrap"
+
+import NovoFilme from "./components/NovoFilme";
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+
+        <h1>My Movies</h1>
+        <button className="btn btn-confirm">Adicionar Filme</button>
+        <table class="table table-sm table-dark">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Nome</th>
+              <th scope="col">Gênero</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Vingadores</td>
+              <td>Aventura</td>
+              <button className="btn btn-danger">X</button>
+            </tr>
+          </tbody>
+        </table>
+
+        <NovoFilme />
+
+      </div>
+    </div >
   );
 }
 
---
+++ src/components/NovoFilme.js
@@ -0,0 +1,43 @@
+import react, { useState } from "react"
+import axios from "axios"
+
+
+const NovoFilme = () => {
+    const [name, setName] = useState([])
+    const [genre, setGenre] = useState('')
+    const genres = ['Ação', 'Aventura', 'Terror', 'Comédia']
+
+    const getChange = evt => {
+        setName(evt.target.value)
+        console.log(evt.target.value)
+    }
+
+    const getChangeGenre = evt => {
+        setGenre(evt.target.value)
+        console.log(evt.target.value)
+    }
+
+    const save = () => {
+        console.log(name, genre)
+    }
+
+
+    return (
+        <div>
+            <label htmlFor="name">Nome</label>
+            <input type='text' className='form-control' onChange={getChange} id='name' placeholder='Nome do Filme' />
+
+            <label for="genre">Selecione o Gênero</label>
+            <select onChange={getChangeGenre} name="genre" id="genre">
+                {
+                    genres.map(g => {
+                        return (<option key={g} value={g}>{g}</option>)
+                    })
+                }
+            </select>
+            <button onClick={save} className="btn btn-primary">Salvar</button>
+        </div >
+    )
+}
+
+export default NovoFilme
